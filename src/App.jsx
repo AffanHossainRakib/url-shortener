1edbbcff0ef650a031fc0e44bae3ef175a400f75
Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-08-03 13:09:52 +0600

    Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl sm:text-7xl flex justify-center text-gray-900">
+        This is my first website using react
+      </h1>
+    </>
+  );
+}
+
+export default App;
