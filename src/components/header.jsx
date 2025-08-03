91185617dc031eee18951f66d5d90f51e702f554
Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-08-03 20:37:24 +0600

    feat: header added

---
+++ src/components/header.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { Link, useNavigate } from "react-router-dom";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const user = true;
+
+  return (
+    <nav className="py-4 flex justify-between items-center">
+      <Link to="/">
+        <img src="/logo.png" alt="Logo" className="h-16" />
+      </Link>
+
+      <div>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger classNam="w-10 rounded-full overflow-hidden">
+              <Avatar className="h-10 w-10">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>AHR</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Md. Affan Hossain Rakib</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2 h-4 w-4" />
+                <span>My LInks</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,15 +1,16 @@
+import Header from "@/components/header";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
-      <main className="min-h-screen container">
-        {/* Header */}
+      <main className="min-h-screen container px-16 max-sm:px-5">
+        <Header />
         <Outlet />
       </main>
 
-      < div className="p-10 text-center bg-gray-800 mt-10">
+      <div className="p-10 text-center bg-gray-800 mt-10">
         Cloned by Affan for Educational Purpose.
       </div>
     </div>
