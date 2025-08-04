d72f0b7cc57b018774fb657f1b3ba560a7e1aaa6
Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-08-04 22:44:27 +0600

    feat: signup API created

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <UrlProvider>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
     </UrlProvider>
   );
 }
